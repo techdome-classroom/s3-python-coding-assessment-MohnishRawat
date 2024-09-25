861ab3016b0d3e760507ccd49d05e3b3681318d4
Mohnish Rawat <rawatmohnish456@gmail.com>
2024-09-25 15:24:03 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: bool
         """
         stack = []
-        mapping = {")": "(", "}": "{", "]": "["}
+        
         for ch in s:
             if ch in '({[':
                 stack.append(ch)
