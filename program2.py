0349fe1238f7f9b3d13b36e7714b66e78799972a
Mohnish Rawat <rawatmohnish456@gmail.com>
2024-09-25 15:32:19 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -5,4 +5,7 @@ class Solution(object):
         :rtype: int
         """
         roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        
+        total=0
+        i=0
+        while(i<len(s)):
+            
