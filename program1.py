5b25d043163d9063b7642d537b1534fae414753d
Mohnish Rawat <rawatmohnish456@gmail.com>
2024-09-25 15:59:45 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -10,10 +10,10 @@ class Solution(object):
             if ch in "({[":
                 stack.append(ch)
             elif ch in ")}]":
-        if not stack:
-            return False
-        top = stack.pop()
-        if (top == "(" and ch != ")") or (top == "{" and ch != "}") or (top == "[" and ch != "]"):
-            return False
+                if not stack:
+                    return False
+                top = stack.pop()
+                if (top == "(" and ch != ")") or (top == "{" and ch != "}") or (top == "[" and ch != "]"):
+                    return False
 
         return not stack
